5d190009949c417ed2586e15a2496f861153befe
Vengatesh Murugasamy <vengateshm.13@gmail.com>
2021-01-05 14:45:31 +0530

    foreground service used to update summary starting from oreo version

--- app/src/main/java/com/vengateshm/covidsummarywidget/COVIDSummaryAppWidgetProvider.kt
+++ app/src/main/java/com/vengateshm/covidsummarywidget/COVIDSummaryAppWidgetProvider.kt
@@ -1,16 +1,18 @@
 package com.vengateshm.covidsummarywidget
 
-import android.app.PendingIntent
-import android.app.Service
+import android.app.*
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import android.view.View
 import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
 import com.vengateshm.covidsummarywidget.models.SummaryResponse
 import com.vengateshm.covidsummarywidget.network.COVIDApiService
 import retrofit2.Call
@@ -69,7 +71,10 @@ class COVIDSummaryAppWidgetProvider : AppWidgetProvider() {
 
     private fun startUpdateService(context: Context) {
         // To prevent any ANR timeouts, we perform the update in a service
-        context.startService(Intent(context, COVIDSummaryUpdateService::class.java))
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
+            context.startService(Intent(context, COVIDSummaryUpdateService::class.java))
+        else
+            context.startForegroundService(Intent(context, COVIDSummaryUpdateService::class.java))
     }
 
     override fun onDisabled(context: Context?) {
@@ -93,13 +98,14 @@ class COVIDSummaryAppWidgetProvider : AppWidgetProvider() {
     companion object {
         class COVIDSummaryUpdateService : Service() {
             private val TAG = "COVIDSummaryUpdateService"
+            private val FOREGROUND_SERVICE_ID = 111
 
             override fun onBind(intent: Intent?): IBinder? {
                 return null
             }
 
             override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-
+                startNotificationForForeground()
                 val views = RemoteViews(
                     this@COVIDSummaryUpdateService.packageName,
                     R.layout.covid_summary_layout
@@ -122,14 +128,24 @@ class COVIDSummaryAppWidgetProvider : AppWidgetProvider() {
                         ) {
                             if (response.isSuccessful && response.code() == 200) {
                                 updateWidgetWithResponse(response.body())
+                            } else {
+                                hideProgressView()
                             }
+                            stopService()
                         }
 
                         override fun onFailure(call: Call<SummaryResponse>, t: Throwable) {
                             t.message?.let { Log.d(TAG, it) }
+                            hideProgressView()
+                            stopService()
                         }
                     })
                 return START_STICKY
+            }
+
+            private fun stopService() {
+                stopForeground(true)
+                stopSelf()
             }
 
             private fun updateWidgetWithResponse(summaryResponse: SummaryResponse?) {
@@ -175,6 +191,67 @@ class COVIDSummaryAppWidgetProvider : AppWidgetProvider() {
                     }
                 } catch (e: Exception) {
                     e.message?.let { Log.d(TAG, it) }
+                    hideProgressView()
+                    stopService()
+                }
+            }
+
+            private fun hideProgressView() {
+                val views = RemoteViews(
+                    this@COVIDSummaryUpdateService.packageName,
+                    R.layout.covid_summary_layout
+                )
+                views.setViewVisibility(R.id.ivRefresh, View.VISIBLE)
+                views.setViewVisibility(R.id.progressBar, View.GONE)
+                val componentName = ComponentName(
+                    this@COVIDSummaryUpdateService.applicationContext,
+                    COVIDSummaryAppWidgetProvider::class.java
+                )
+                val manager =
+                    AppWidgetManager.getInstance(this@COVIDSummaryUpdateService.applicationContext)
+                manager.updateAppWidget(componentName, views)
+            }
+
+            private fun startNotificationForForeground() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForeground(
+                        FOREGROUND_SERVICE_ID,
+                        createNotification(
+                            "COVID19Service", "COVID19 Summary Channel",
+                            null,
+                            getString(R.string.foreground_not_text)
+                        )
+                    )
+                }
+            }
+
+            @RequiresApi(Build.VERSION_CODES.O)
+            private fun createNotification(
+                channelId: String,
+                channelName: String,
+                contentTitle: String?,
+                contentText: String,
+                pendingIntent: PendingIntent? = null
+            ): Notification {
+                val notificationChannel =
+                    NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_NONE)
+                notificationChannel.description = channelId
+                notificationChannel.setSound(null, null)
+                notificationChannel.lightColor = Color.BLUE
+                notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+
+                val notificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                return Notification.Builder(this, channelId).let { builder ->
+                    contentTitle?.let {
+                        builder.setContentTitle(contentTitle)
+                    }
+                    builder.setContentText(contentText)
+                    builder.setSmallIcon(R.drawable.ic_covid_19_33)
+                    pendingIntent?.let { builder.setContentIntent(it) }
+                    builder.build()
                 }
             }
         }
